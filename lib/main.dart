47472cb8fbafda438a18dbfed443e101a5496e66
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-17 18:08:02 -0300

    chore: iniciando projeto com getX e com firebase

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:app_barber_shop/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async{
+  await GetStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "App Barber shop",
+      home: const Center(),
+      builder: EasyLoading.init(),
+    );
+  }
+}
