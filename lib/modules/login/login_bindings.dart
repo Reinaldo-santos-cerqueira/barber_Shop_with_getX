f3716716313b929b2d0c130a704ae64cbe1971f6
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-21 18:07:46 -0300

    feat: inicializando o controller

--- lib/modules/login/login_bindings.dart
+++ lib/modules/login/login_bindings.dart
@@ -1,7 +1,11 @@
+import 'package:app_barber_shop/modules/login/login_controller.dart';
 import 'package:get/get.dart';
 
 class LoginBindings implements Bindings {
   @override
   void dependencies() {
+    Get.put(
+      LoginController(),
+    );
   }
 }
