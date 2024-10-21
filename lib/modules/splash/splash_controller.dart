eac1b70d50509f327960a9c116f4032294ca9438
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-21 15:40:50 -0300

    feat: salvando a linguagem padrao do aplicativo

--- lib/modules/splash/splash_controller.dart
+++ lib/modules/splash/splash_controller.dart
@@ -2,21 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
-class SplashController extends GetxController  {
+class SplashController extends GetxController {
   @override
   Future<void> onInit() async {
     super.onInit();
     await getVersion();
   }
+
   Future<void> getVersion() async {
+    await getLanguage();
     await Future.delayed(const Duration(seconds: 1));
-    // Get.offAllNamed('/preLogin');
+    Get.offAllNamed('/preLogin');
   }
 
-  Future<void> getLanguage(context) async{
-    Locale currentLocale = Localizations.localeOf(context);
+  Future<void> getLanguage() async {
+    Locale? currentLocale = Get.deviceLocale;
+    String? language = currentLocale?.languageCode.toString();
     final box = GetStorage();
-    box.write('language', currentLocale);
 
+
+    box.write('language', language);
   }
 }
