f1773741e1843adf67e67f6e4fc6204289ec95c8
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-21 18:07:04 -0300

    feat: adicionando mais uma cor ao projeto

--- lib/application/theme/colors_project.dart
+++ lib/application/theme/colors_project.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
 class ColorsProject {
-  static const primaryColor = const Color(0xFFC59D5F);
+  static const primaryColor = Color(0xFFC59D5F);
+  static const bgColor = Color(0xFF121212);
 }
