9cf75e09c2c2a82351c12a4861f661e081d38630
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-21 15:45:53 -0300

    feat: implementando elemento para  ser o botão

---
+++ lib/widgets/button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../application/theme/colors_project.dart';
+import '../utils/get_size.dart';
+
+class Button extends StatelessWidget {
+  final bool? primaryButton;
+  final String textBtn;
+  final Function onPressedFunction;
+  const Button({super.key, this.primaryButton, required this.textBtn, required this.onPressedFunction});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: getSize(context).width * .8,
+      height: 50.0,
+      child: ElevatedButton(
+        onPressed: () {
+          onPressedFunction();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: primaryButton == true ? ColorsProject.primaryColor : Colors.transparent,
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(color: ColorsProject.primaryColor),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Text(
+          textBtn.toUpperCase(),
+          style: TextStyle(
+              fontSize: 24,
+              color: primaryButton == true ? Colors.black : ColorsProject.primaryColor,
+              fontWeight: FontWeight.bold,
+            letterSpacing: 5
+          ),
+        ),
+      ),
+    );
+  }
+}
