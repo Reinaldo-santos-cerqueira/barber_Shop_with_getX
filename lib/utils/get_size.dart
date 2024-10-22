54d912bc7d4ee256d16d54af8953b13d6faba2c8
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-22 13:15:53 -0300

    feat: transformando em classe e adicionando uma outra função

--- lib/utils/get_size.dart
+++ lib/utils/get_size.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
-Size getSize(context) {
-  return MediaQuery.of(context).size;
+class GetSize {
+  Size sizeScreen(context) {
+    return MediaQuery.of(context).size;
+  }
+
+  double getHeightWithoutAppBar(context){
+    double screenHeight = MediaQuery.of(context).size.height;
+    double appBarHeight = AppBar().preferredSize.height;
+    return screenHeight - appBarHeight;
+  }
 }
