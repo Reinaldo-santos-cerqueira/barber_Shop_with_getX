e4a812ba4a2ad55b62f718e5e183154440233a72
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-22 13:18:31 -0300

    feat: adicionando inputs a tela de login

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:app_barber_shop/modules/login/login_controller.dart';
 import 'package:app_barber_shop/utils/get_size.dart';
 import 'package:app_barber_shop/widgets/button.dart';
 import 'package:app_barber_shop/widgets/icon_back.dart';
+import 'package:app_barber_shop/widgets/input.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,6 +12,11 @@ class LoginScreen extends GetView<LoginController> {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    var size = GetSize();
+    var getSize = size.sizeScreen(context);
+
     return Obx(() {
       return Scaffold(
         appBar: AppBar(
@@ -19,76 +25,117 @@ class LoginScreen extends GetView<LoginController> {
         ),
         backgroundColor: ColorsProject.bgColor,
         body: Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: getSize(context).width * .1),
+          padding: EdgeInsets.symmetric(horizontal: getSize.width * .1),
           child: SizedBox(
             width: Size.infinite.width,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.end,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Button(
-                  textBtn: controller.textBtnLogin.value.toUpperCase(),
-                  primaryButton: true,
-                  onPressedFunction: () {
-                    print(123);
-                  },
-                ),
-                Row(
-                  children: [
-                    Text(
-                      controller.textProblem.value,
-                      style: const TextStyle(
-                          color: ColorsProject.primaryColor, fontSize: 18),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(controller.textBtnClickMe.value,
-                          style: const TextStyle(
+            height: size.getHeightWithoutAppBar(context),
+            child: Form(
+              child: SingleChildScrollView(
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  height: size.getHeightWithoutAppBar(context),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min, 
+                    children: [
+                      Input(
+                        passwordInput: false,
+                        textHint: "Digite seu email",
+                        controller: emailController,
+                        icon: Icons.alternate_email,
+                      ),
+                      const SizedBox(height: 30),
+                      Input(
+                        passwordInput: true,
+                        textHint: "Digite sua senha",
+                        controller: passwordController,
+                        icon: Icons.lock,
+                      ),
+                      const SizedBox(height: 30),
+                      Button(
+                        textBtn: controller.textBtnLogin.value.toUpperCase(),
+                        primaryButton: true,
+                        onPressedFunction: () {
+                          print(123);
+                        },
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            controller.textProblem.value,
+                            style: const TextStyle(
                               color: ColorsProject.primaryColor,
                               fontSize: 18,
-                            decoration: TextDecoration.underline
-                          )
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              padding: MaterialStateProperty.all(
+                                const EdgeInsets.only(
+                                    top: 0, left: 5, bottom: 0, right: 0),
+                              ),
+                            ),
+                            child: Container(
+                              padding: const EdgeInsets.all(0),
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: ColorsProject.primaryColor,
+                                    width: 1.0,
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                controller.textBtnClickMe.value,
+                                style: const TextStyle(
+                                  color: ColorsProject.primaryColor,
+                                  fontSize: 18,
+                                  decoration: TextDecoration.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        color: ColorsProject.primaryColor,
-                        height: 1,
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              color: ColorsProject.primaryColor,
+                              height: 1,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            controller.textOr.value.toUpperCase(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: ColorsProject.primaryColor,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Container(
+                              color: ColorsProject.primaryColor,
+                              height: 1,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    const SizedBox(width: 10),
-                    Text(
-                      controller.textOr.value.toUpperCase(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: ColorsProject.primaryColor),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: Container(
-                        color: ColorsProject.primaryColor,
-                        height: 1,
+                      const SizedBox(height: 20),
+                      Button(
+                        textBtn: controller.textBtnSignUp.value.toUpperCase(),
+                        onPressedFunction: () {
+                          print(123);
+                        },
                       ),
-                    )
-                  ],
+                      const SizedBox(height: 50),
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 20),
-                Button(
-                  textBtn: controller.textBtnSignUp.value.toUpperCase(),
-                  onPressedFunction: () {
-                    print(123);
-                  },
-                ),
-                const SizedBox(height: 50),
-              ],
+              ),
             ),
           ),
         ),
---
+++ lib/widgets/input.dart
@@ -0,0 +1,82 @@
+import 'package:app_barber_shop/utils/get_size.dart';
+import 'package:flutter/material.dart';
+
+import '../application/theme/colors_project.dart';
+
+class Input extends StatefulWidget {
+  final String textHint;
+  final TextEditingController controller;
+  final IconData icon;
+  final bool passwordInput;
+
+  const Input(
+      {super.key,
+      required this.textHint,
+      required this.controller,
+      required this.icon,
+      required this.passwordInput});
+
+  @override
+  State<Input> createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  bool obscureText = false;
+
+  changeObscureText() {
+    setState(() {
+      obscureText = !obscureText;
+    });
+  }
+
+  @override
+  void initState() {
+    widget.passwordInput == true ? obscureText = true : obscureText = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var getSize =  GetSize().sizeScreen(context);
+    return Container(
+      width: getSize.width,
+      height: 50,
+      decoration: const BoxDecoration(
+        color: ColorsProject.bgInput,
+        borderRadius: BorderRadius.all(
+          Radius.circular(5),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(widget.icon, color: ColorsProject.primaryColor),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextFormField(
+                controller: widget.controller,
+                obscureText: obscureText,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: widget.textHint,
+                    hintStyle: const TextStyle(
+                      color: ColorsProject.primaryColor,
+                    )),
+              ),
+            ),
+            const SizedBox(width: 10),
+            widget.passwordInput == true
+                ? (obscureText == true
+                    ? const Icon(Icons.visibility_off,
+                        color: ColorsProject.primaryColor)
+                    : const Icon(Icons.visibility,
+                        color: ColorsProject.primaryColor))
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
