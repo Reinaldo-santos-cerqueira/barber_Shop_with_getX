c715f1ce093e93ab464b183af4f8be33d96917b7
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-22 13:15:16 -0300

    feat: implementando uma nova cor

--- lib/application/theme/colors_project.dart
+++ lib/application/theme/colors_project.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class ColorsProject {
   static const primaryColor = Color(0xFFC59D5F);
   static const bgColor = Color(0xFF121212);
+  static const bgInput = Color(0xFF1B1B1B);
 }
