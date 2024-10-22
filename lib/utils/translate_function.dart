ae316f403811b6c64acaef03c00d9d8ca881016e
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-10-22 13:16:28 -0300

    style: usando o format

--- lib/utils/translate_function.dart
+++ lib/utils/translate_function.dart
@@ -1,10 +1,11 @@
 import 'package:get_storage/get_storage.dart';
 import 'package:translator/translator.dart';
 
-Future<String> translateFunction(String textOriginal) async{
+Future<String> translateFunction(String textOriginal) async {
   final box = GetStorage();
   final translator = GoogleTranslator();
 
-  var translateText = await translator.translate(textOriginal,from: 'auto',to:box.read('language'));
+  var translateText = await translator.translate(textOriginal,
+      from: 'auto', to: box.read('language'));
   return translateText.toString();
 }
