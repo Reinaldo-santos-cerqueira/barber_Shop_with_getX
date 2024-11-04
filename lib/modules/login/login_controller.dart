b1429ca6612289d82bbd9e15df472110aa838c99
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-11-04 18:21:49 -0300

    feat: add new props and validators

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,18 +1,24 @@
+import 'package:app_barber_shop/services/firebase/messaging/firebase_messaging.dart';
 import 'package:app_barber_shop/utils/translate_function.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
-  final textBtnLogin = "login".obs;
-  final textBtnSignUp = "sign up".obs;
-  final textProblem = "Problemas com login?".obs;
-  final textBtnClickMe = "Clique aqui".obs;
-  final placeHolderEmail = "Digite seu email".obs;
-  final placeHolderPassword = "Digite seu senha".obs;
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+
   final textOr = "or".obs;
   final seePassword = false.obs;
+  final textBtnLogin = "login".obs;
+  final textBtnSignUp = "sign up".obs;
+  final loadingButton = false.obs;
+  final formKey = GlobalKey<FormState>();
+  final textBtnClickMe = "Clique aqui".obs;
+  final textProblem = "Problemas com login?".obs;
+  final placeHolderEmail = "Digite seu email".obs;
+  final placeHolderPassword = "Digite seu senha".obs;
+  final messageErroEmail = "Insira um email valido".obs;
+  final messageErroPassword = "A senha precisa ser forte".obs;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Future<void> onInit() async {
@@ -24,5 +30,15 @@ class LoginController extends GetxController {
     placeHolderEmail(await translateFunction(placeHolderEmail.value));
     placeHolderPassword(await translateFunction(placeHolderPassword.value));
     textOr(await translateFunction(textOr.value));
+    messageErroEmail(await translateFunction(messageErroEmail.value));
+    messageErroPassword(await translateFunction(messageErroPassword.value));
+  }
+
+  login() async {
+    await MessagingFirebase().getToken();
+  }
+
+  goSignUp() {
+    Get.toNamed('/signup');
   }
 }
--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -7,6 +7,7 @@ import 'package:app_barber_shop/widgets/input.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:line_icons/line_icon.dart';
+import 'package:validatorless/validatorless.dart';
 
 class LoginScreen extends GetView<LoginController> {
   const LoginScreen({super.key});
@@ -32,117 +33,141 @@ class LoginScreen extends GetView<LoginController> {
                 child: SafeArea(
                   child: SizedBox(
                     width: double.infinity,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Expanded(
-                          child: Center(
-                            child: Image.asset(
-                              'assets/images/logo.png',
-                              width: 250,
+                    child: Form(
+                      key: controller.formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Expanded(
+                            child: Center(
+                              child: Image.asset(
+                                'assets/images/logo.png',
+                                width: 250,
+                              ),
                             ),
                           ),
-                        ),
-                        Input(
-                          textHint: controller.placeHolderEmail.value,
-                          controller: controller.emailController,
-                          icon: const Icon(Icons.alternate_email, color: ColorsProject.primaryColor,),
-                          passwordInput: false,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Input(
-                          textHint: controller.placeHolderPassword.value,
-                          controller: controller.passwordController,
-                          icon: const LineIcon.lock(color: ColorsProject.primaryColor,),
-                          passwordInput: true,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Row(
-                            children: [
-
-                              Text(
-                                controller.textProblem.value,
-                                style: const TextStyle(
-                                  color: ColorsProject.primaryColor,
-                                ),
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                style: ButtonStyle(
-                                  padding: WidgetStateProperty.all(
-                                    const EdgeInsets.only(
-                                        top: 0, left: 5, bottom: 0, right: 0),
+                          Input(
+                            textHint: controller.placeHolderEmail.value,
+                            controller: controller.emailController,
+                            icon: const Icon(
+                              Icons.alternate_email,
+                              color: ColorsProject.primaryColor,
+                            ),
+                            passwordInput: false,
+                            validator: Validatorless.email(
+                                controller.messageErroEmail.value),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Input(
+                            textHint: controller.placeHolderPassword.value,
+                            controller: controller.passwordController,
+                            icon: const LineIcon.lock(
+                              color: ColorsProject.primaryColor,
+                            ),
+                            passwordInput: true,
+                            validator: Validatorless.regex(
+                                RegExp(
+                                    r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$'),
+                                controller.messageErroPassword.value),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  controller.textProblem.value,
+                                  style: const TextStyle(
+                                    color: ColorsProject.primaryColor,
                                   ),
                                 ),
-                                child: Container(
-                                  padding: const EdgeInsets.all(0),
-                                  decoration: const BoxDecoration(
-                                    border: Border(
-                                      bottom: BorderSide(
+                                TextButton(
+                                  onPressed: () {},
+                                  style: ButtonStyle(
+                                    padding: WidgetStateProperty.all(
+                                      const EdgeInsets.only(
+                                          top: 0, left: 5, bottom: 0, right: 0),
+                                    ),
+                                  ),
+                                  child: Container(
+                                    padding: const EdgeInsets.all(0),
+                                    decoration: const BoxDecoration(
+                                      border: Border(
+                                        bottom: BorderSide(
+                                          color: ColorsProject.primaryColor,
+                                          width: 1.0,
+                                        ),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      controller.textBtnClickMe.value,
+                                      style: const TextStyle(
                                         color: ColorsProject.primaryColor,
-                                        width: 1.0,
+                                        decoration: TextDecoration.none,
                                       ),
                                     ),
                                   ),
-                                  child: Text(
-                                    controller.textBtnClickMe.value,
-                                    style: const TextStyle(
-                                      color: ColorsProject.primaryColor,
-                                      decoration: TextDecoration.none,
-                                    ),
-                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Obx(() {
+                            return Button(
+                              textBtn:
+                                  controller.textBtnLogin.value.toUpperCase(),
+                              primaryButton: true,
+                              onPressedFunction: () {
+                                controller.login();
+                              },
+                              loading: controller.loadingButton.obs,
+                            );
+                          }),
+                          const SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  color: ColorsProject.primaryColor,
+                                  height: 1,
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              Text(
+                                controller.textOr.value.toUpperCase(),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: ColorsProject.primaryColor,
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: Container(
+                                  color: ColorsProject.primaryColor,
+                                  height: 1,
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Button(
-                          textBtn: controller.textBtnLogin.value.toUpperCase(),
-                          primaryButton: true,
-                          onPressedFunction: () {
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Container(
-                                color: ColorsProject.primaryColor,
-                                height: 1,
-                              ),
-                            ),
-                            const SizedBox(width: 10),
-                            Text(
-                              controller.textOr.value.toUpperCase(),
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: ColorsProject.primaryColor,
-                              ),
-                            ),
-                            const SizedBox(width: 10),
-                            Expanded(
-                              child: Container(
-                                color: ColorsProject.primaryColor,
-                                height: 1,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        Button(
-                          textBtn: controller.textBtnSignUp.value.toUpperCase(),
-                          onPressedFunction: () {
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                      ],
+                          const SizedBox(height: 20),
+                          Obx(() {
+                            return Button(
+                              textBtn:
+                                  controller.textBtnSignUp.value.toUpperCase(),
+                              onPressedFunction: () {
+                                controller.goSignUp();
+                              },
+                              loading: Rx<RxBool>(false.obs),
+                            );
+                          }),
+                          const SizedBox(height: 20),
+                        ],
+                      ),
                     ),
                   ),
                 ),
