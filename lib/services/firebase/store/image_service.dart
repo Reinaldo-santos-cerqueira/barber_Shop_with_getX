6646f7a4a306eb2d64747ef648b1e52d56eac612
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-11-06 13:38:11 -0300

    feat: add new logic to upload image

--- lib/services/firebase/store/image_service.dart
+++ lib/services/firebase/store/image_service.dart
@@ -2,15 +2,36 @@ import 'dart:io';
 
 import 'package:app_barber_shop/models/user_model.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ImageService {
-  Future<String> download(UserModel user, String uid) async {
-    Reference storageRef = FirebaseStorage.instance.ref().child('user_images/$uid.jpg');
-    UploadTask uploadTask = storageRef.putFile(File(user.photo));
-    await uploadTask;
+  Future<String?> uploadImage(UserModel user, String uid) async {
+    final firebaseStorage = FirebaseStorage.instance;
+    await requestStoragePermission();
 
-    String downloadUrl = await storageRef.getDownloadURL();
-    
+    if (user.photo == null) {
+      throw Exception("Arquivo de imagem não fornecido.");
+    }
+
+    if (user.photo == null || !File(user.photo!.path).existsSync()) {
+      throw Exception("Imagem não encontrada ou caminho inválido.");
+    }
+
+    String filePath = 'uploaded_images/${uid}_${DateTime.now().millisecondsSinceEpoch}.png';
+
+    File file = File(user.photo!.path);
+
+    await firebaseStorage.ref(filePath).putFile(file);
+
+    String downloadUrl = await firebaseStorage.ref(filePath).getDownloadURL();
+
     return downloadUrl;
+  }
+
+  Future<void> requestStoragePermission() async {
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
   }
 }
