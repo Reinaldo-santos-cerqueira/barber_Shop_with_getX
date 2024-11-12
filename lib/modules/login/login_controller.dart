c0550b55e20b014de6aa755bd15c6f8c68dc2eb5
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-11-12 12:12:30 -0300

    feat: implementando a logica de login

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,15 +1,18 @@
-import 'package:app_barber_shop/services/firebase/messaging/firebase_messaging.dart';
+import 'package:app_barber_shop/application/theme/colors_project.dart';
+import 'package:app_barber_shop/services/firebase/store/user_service.dart';
 import 'package:app_barber_shop/utils/translate_function.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quickalert/quickalert.dart';
 
 class LoginController extends GetxController {
-
   final textOr = "or".obs;
   final seePassword = false.obs;
+  final loadingButton = false.obs;
+  final loadingScreen = false.obs;
   final textBtnLogin = "login".obs;
   final textBtnSignUp = "sign up".obs;
-  final loadingButton = false.obs;
   final formKey = GlobalKey<FormState>();
   final textBtnClickMe = "Clique aqui".obs;
   final textProblem = "Problemas com login?".obs;
@@ -23,6 +26,7 @@ class LoginController extends GetxController {
   @override
   Future<void> onInit() async {
     super.onInit();
+    loadingScreen(true);
     textBtnLogin(await translateFunction(textBtnLogin.value));
     textBtnSignUp(await translateFunction(textBtnSignUp.value));
     textProblem(await translateFunction(textProblem.value));
@@ -32,10 +36,65 @@ class LoginController extends GetxController {
     textOr(await translateFunction(textOr.value));
     messageErroEmail(await translateFunction(messageErroEmail.value));
     messageErroPassword(await translateFunction(messageErroPassword.value));
+    loadingScreen(false);
   }
 
-  login() async {
-    await MessagingFirebase().getToken();
+  login(context) async {
+    loadingButton(true);
+    if (!formKey.currentState!.validate()) {
+      loadingButton(false);
+      return;
+    }
+    try {
+      await UserService().signIn(emailController.text, passwordController.text);
+      QuickAlert.show(
+          context: context,
+          type: QuickAlertType.success,
+          backgroundColor: ColorsProject.bgColor,
+          titleColor: Colors.white,
+          textColor: Colors.white,
+          confirmBtnColor: Colors.transparent,
+          text: await translateFunction('Successful login!'),
+          onConfirmBtnTap: () {
+            Get.back(); 
+            Get.offAllNamed("/home");
+          });
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.error,
+          backgroundColor: ColorsProject.bgColor,
+          titleColor: Colors.white,
+          textColor: Colors.white,
+          confirmBtnColor: Colors.transparent,
+          text: await translateFunction('Credencials invalid!'),
+        );
+      } else if (e.code == 'wrong-password') {
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.error,
+          backgroundColor: ColorsProject.bgColor,
+          titleColor: Colors.white,
+          textColor: Colors.white,
+          confirmBtnColor: Colors.transparent,
+          text: await translateFunction('Credencials invalid!'),
+        );
+      }
+    } catch (e) {
+      QuickAlert.show(
+        context: context,
+        type: QuickAlertType.error,
+        backgroundColor: ColorsProject.bgColor,
+        titleColor: Colors.white,
+        textColor: Colors.white,
+        confirmBtnColor: Colors.transparent,
+        text: await translateFunction(
+            'Internal server error.\n Try again in a little while'),
+      );
+    } finally {
+      loadingButton(false);
+    }
   }
 
   goSignUp() {
