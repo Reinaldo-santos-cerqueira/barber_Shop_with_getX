8a2da55cd3e7be0e286e44e83a2dfce14a71048c
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-11-12 12:13:13 -0300

    feat: implementando novas telas e funções

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:app_barber_shop/firebase_options.dart';
+import 'package:app_barber_shop/modules/home/home_module.dart';
 import 'package:app_barber_shop/modules/login/login_module.dart';
 import 'package:app_barber_shop/modules/pre_login/pre_login_module.dart';
 import 'package:app_barber_shop/modules/signup/signup_module.dart';
 import 'package:app_barber_shop/modules/splash/splash_module.dart';
+import 'package:app_barber_shop/services/firebase/check_valid.dart';
 import 'package:app_barber_shop/services/firebase/messaging/firebase_messaging.dart';
 import 'package:app_barber_shop/services/permissions/get_permissions.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -21,6 +23,7 @@ void main() async {
   await MessagingFirebase().initNotification();
   await requestStoragePermission(Permission.storage);
   await requestCameraAndStoragePermissions();
+  await checkGooglePlayServices();
   runApp(const MyApp());
 }
 
@@ -36,6 +39,7 @@ class MyApp extends StatelessWidget {
         ...LoginModule().routers,
         ...PreLoginModule().routers,
         ...SignupModule().routers,
+        ...HomeModule().routers
       ],
       builder: EasyLoading.init(),
     );
